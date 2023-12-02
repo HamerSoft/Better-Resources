be5cd9514c4f6f5d5120b0d0fcf1d5212358bcd3
RHamers <ruben_hamers@hotmail.com>
2023-12-02 11:27:54 +0000

    Deploying to gh-pages from @ HamerSoft/better-resources@0b20677a5906eae4a7c1da3addd5990756b50293 🚀

--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['0_2e1_2e0_20_282023_2d11_2d26_29_131',['0.1.0 (2023-11-26)',['../md_CHANGELOG.html',1,'']]]
+  ['_3ca_20href_3d_22https_3a_2f_2fgithub_2ecom_2fhamersoft_2fbetter_2dresources_2fcompare_2fv0_2e1_2e0_2e_2e_2ev0_2e2_2e0_22_3e0_2e2_2e0_3c_2fa_3e_20_282023_2d12_2d02_29_131',['&lt;a href=&quot;https://github.com/HamerSoft/better-resources/compare/v0.1.0...v0.2.0&quot;&gt;0.2.0&lt;/a&gt; (2023-12-02)',['../md_CHANGELOG.html',1,'']]]
 ];
--- search/searchdata.js
+++ search/searchdata.js
@@ -1,12 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "0abcfghiloqrtuw",
+  0: "<abcfghiloqrtuw",
   1: "ablqrt",
   2: "h",
   3: "abfgiloqrtuw",
   4: "i",
   5: "ci",
-  6: "0bl"
+  6: "<bl"
 };
 
 var indexSectionNames =
